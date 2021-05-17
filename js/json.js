847ebcd612c3a1a4a683ef856310d9a8a0e68336
Saif <asaifindia@gmail.com>
2021-05-17 07:32:52 +0530

    moved newsleter sec data to a js file.

--- js/json.js
+++ js/json.js
@@ -41,3 +41,40 @@ function getPhotodiaryData(){
     message: "Leave a comment",
   };
 }
+
+function getBlogSecData(){
+  return [
+    {
+      blog_img: "images/blog-img1.jpg",
+      about: "Lifestyle",
+      heading: "More than just a music festival",
+      paragraph: `Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea`,
+    },
+    {
+      blog_img: "images/blog-img2.jpg",
+      about: "Lifestyle",
+      heading: "Life tastes better with coffee",
+      paragraph: `Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea`,
+    },
+    {
+      blog_img: "images/blog-img3.jpg",
+      about: "Lifestyle",
+      heading: "American dream",
+      paragraph: `Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea`,
+    },
+    {
+      blog_img: "images/blog-img4.jpg",
+      about: "Lifestyle",
+      heading: "A day exploring the Alps",
+      paragraph: `Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea`,
+    },
+  ];
+}
+
+function getNewsData(){
+  return "Sign up for our newsletter!";
+}
+
+function getNewsData(){
+  return "Sign up for our newsletter!";
+}
