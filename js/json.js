c2a926ba4e3532ed92b12e26c9c22a66023c492e
Saif <asaifindia@gmail.com>
2021-05-17 07:23:36 +0530

    moved header data to a js file.

---
+++ js/json.js
@@ -0,0 +1,27 @@
+function getLogoData(){
+  return {
+    image: "images/logo.png",
+    url: "index.html",
+  }
+}
+
+function getNavData(){
+  return [
+    {
+      name: "Lifestyle",
+      url: "#",
+    },
+    {
+      name: "Photodiary",
+      url: "#",
+    },
+    {
+      name: "Music",
+      url: "#",
+    },
+    {
+      name: "Travel",
+      url: "#",
+    },
+  ];
+}
